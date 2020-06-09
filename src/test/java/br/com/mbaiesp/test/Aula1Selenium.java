8a4cbca4c0dd47c44ae4cfee43927ff62d30d848
Rodrigo Barbosa <rodrigo.barba.88@gmail.com>
2020-06-09 20:22:33 -0300

    first commit to repository.

--- src/test/java/br/com/mbaiesp/test/Aula1Selenium.java
+++ src/test/java/br/com/mbaiesp/test/Aula1Selenium.java
@@ -3,6 +3,7 @@ package br.com.mbaiesp.test;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
@@ -25,8 +26,12 @@ public class Aula1Selenium {
 	
 	@Test
 	public void teste01() {
-		
 		PageFactory.initElements(this.driver, this);
 		driver.navigate().to("https://www.google.com/");
+		driver.findElement(By.name("q"));
+		String text = "recursion";
+		driver.findElement(By.name("q")).sendKeys(text);
+		driver.findElement(By.name("btnK")).submit();
+		driver.findElement(By.xpath("//*[@id=\"taw\"]/div[2]/div/p/a/b/i")).getText();
 	}
 }
